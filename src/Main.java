c3f19f287599cdfc21abf6b169c9b17527007805
Юрий <fedejkin@gmail.com>
2023-02-08 14:01:06 +0300

    Commit-Master

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+        int a = 2146483647;
+        byte b = 126;
+        short c = 32765;
+        long d = 2147483646;
+        float e = 1.2034571F;
+        double f = 5.23456578985689;
+        System.out.println("Значение переменной a с типом int равно " + a);
+        System.out.println("Значение переменной b с типом byte равно " + b);
+        System.out.println("Значение переменной c с типом short равно " + c);
+        System.out.println("Значение переменной d с типом long равно " + d);
+        System.out.println("Значение переменной e с типом float равно " + e);
+        System.out.println("Значение переменной f с типом double равно " + f);
+    }
+}
